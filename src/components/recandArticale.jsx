ff798c459bddcad3bc2aeb1a26987ed60ff722a1
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-07-16 14:10:03 +0500

    updaring the existing code of the artical file

--- src/components/recandArticale.jsx
+++ src/components/recandArticale.jsx
@@ -9,7 +9,7 @@ const articles = [
         date: "2024-01-10",
         readTime: "7 min read",
         image:
-            "https://images.unsplash.com/photo-1542351515-64c6b4db38b4?q=80&w=1029&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // example AI image
+            "https://images.unsplash.com/photo-1531746790731-6c087fecd65a?q=80&w=806&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // example AI image
     },
     {
         category: "Automation",
@@ -18,7 +18,7 @@ const articles = [
         date: "2024-01-05",
         readTime: "6 min read",
         image:
-            "https://images.unsplash.com/photo-1557446772-bea6e6bf56eb?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // people at work
+            "https://images.unsplash.com/photo-1519389950473-47ba0277781c?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // people at work
     },
     {
         category: "3D Technology",
@@ -27,25 +27,25 @@ const articles = [
         date: "2023-12-28",
         readTime: "4 min read",
         image:
-            "https://images.unsplash.com/photo-1635351610543-752b8abe48aa?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // modern interior
+            "https://images.unsplash.com/photo-1658806312303-6815563cd9d8?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // modern interior
     },
     {
-        category: "3D Technology",
-        title: "3D Visualization in E-commerce",
-        desc: "How 3D product visualization is transforming online shopping experiences and boosting conversion rates.",
+        category: "Design",
+        title: "UI/UX Trends for 2024",
+        desc: "The latest design trends and user experience principles that will dominate the digital landscape this year.",
         date: "2023-12-28",
         readTime: "4 min read",
         image:
-            "https://images.unsplash.com/photo-1635351610543-752b8abe48aa?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // modern interior
+            "https://images.unsplash.com/photo-1545235617-9465d2a55698?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // modern interior
     },
     {
-        category: "3D Technology",
-        title: "3D Visualization in E-commerce",
-        desc: "How 3D product visualization is transforming online shopping experiences and boosting conversion rates.",
+        category: "Voice AI",
+        title: "Voice AI: The Next Frontier",
+        desc: "Exploring the potential of voice AI technology and its applications in modern business environments.",
         date: "2023-12-28",
         readTime: "4 min read",
         image:
-            "https://images.unsplash.com/photo-1635351610543-752b8abe48aa?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // modern interior
+            "https://images.unsplash.com/photo-1678977252570-58db7acbbeea?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // modern interior
     },
 ];
 
--- src/page/blog.jsx
+++ src/page/blog.jsx
@@ -60,6 +60,13 @@ const blog = () => {
             </div>
             <div className='mt-12'>
                 <RecentArticles />
+                <div className='bg-gradient-to-r from-[#007BFF] to-[#00FFD1] text-center py-10'>
+                    <h1 className='text-4xl text-white font-semibold'>Stay Updated with AI & Tech Trends</h1>
+                    <p className='text-white text-lg leading-loose py-6'>
+                        Get weekly insights, tips, and industry updates delivered to your inbox.
+                    </p>
+                    <button class="hover:bg-gradient-to-r from-[#007BFF] to-[#00FFD1] hover:text-white text-gray-700 font-semibold py-2 px-4 rounded-lg bg-white  hover:opacity-90 transition">Subcribe Now</button>
+                </div>
             </div>
         </div>
     )
