7bb462a52ac840c0661249685c097c3d59099867
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-07-17 14:44:39 +0500

    updating the navbar code

--- src/components/serviceDrapDownMenu.jsx
+++ src/components/serviceDrapDownMenu.jsx
@@ -12,49 +12,49 @@ const serviceDrapDownMenu = ({ isServiceOpen }) => {
                     <li className="px-4 py-2 hover:bg-gray-100 rounded-md">
                         <Link to="/service/web">
                             <h1 className='text-gray-700 leading-loose font-bold '>AI & Generative AI</h1>
-                            <p className='text-sm text-gray-400 font-medium'>Chatbot , LLMs , Custome AI Apps</p>
+                            <p className='text-sm text-gray-400'>Chatbot , LLMs , Custome AI Apps</p>
                         </Link>
                     </li>
                     <li className="px-4 py-2 hover:bg-gray-100 rounded-md">
                         <Link to="/service/web">
                             <h1 className='text-gray-700 leading-loose font-bold '>AI + Workflow Automation</h1>
-                            <p className='text-sm text-gray-400 font-medium'>Zapier , Make , n8n Integration</p>
+                            <p className='text-sm text-gray-400'>Zapier , Make , n8n Integration</p>
                         </Link>
                     </li>
                     <li className="px-4 py-2 hover:bg-gray-100 rounded-md">
                         <Link to="/service/web">
                             <h1 className='text-gray-700 leading-loose font-bold '>Machine Learning Data Intelligence</h1>
-                            <p className='text-sm text-gray-400 font-medium'>Predictive Analysis , Dashboards</p>
+                            <p className='text-sm text-gray-400'>Predictive Analysis , Dashboards</p>
                         </Link>
                     </li>
                     <li className="px-4 py-2 hover:bg-gray-100 rounded-md">
                         <Link to="/service/web">
                             <h1 className='text-gray-700 leading-loose font-bold '>Voice & Speech AI</h1>
-                            <p className='text-sm text-gray-400 font-medium'>Voicebots , IVR , AI Avatars</p>
+                            <p className='text-sm text-gray-400'>Voicebots , IVR , AI Avatars</p>
                         </Link>
                     </li>
                     <li className="px-4 py-2 hover:bg-gray-100 rounded-md">
                         <Link to="/service/web">
                             <h1 className='text-gray-700 leading-loose font-bold '>3D Modeling & AR/VR</h1>
-                            <p className='text-sm text-gray-400 font-medium'>Characters , Product Renders , NFTs</p>
+                            <p className='text-sm text-gray-400'>Characters , Product Renders , NFTs</p>
                         </Link>
                     </li>
                     <li className="px-4 py-2 hover:bg-gray-100 rounded-md">
                         <Link to="/service/web">
                             <h1 className='text-gray-700 leading-loose font-bold '>UI/UX & Frontend Development</h1>
-                            <p className='text-sm text-gray-400 font-medium'>Web/Mobile Design</p>
+                            <p className='text-sm text-gray-400'>Web/Mobile Design</p>
                         </Link>
                     </li>
                     <li className="px-4 py-2 hover:bg-gray-100 rounded-md">
                         <Link to="/service/web">
                             <h1 className='text-gray-700 leading-loose font-bold '>Python & Custome Automation</h1>
-                            <p className='text-sm text-gray-400 font-medium'>Web Scraping , APIs , Beckend</p>
+                            <p className='text-sm text-gray-400'>Web Scraping , APIs , Beckend</p>
                         </Link>
                     </li>
                     <li className="px-4 py-2 hover:bg-gray-100 rounded-md">
                         <Link to="/service/web">
                             <h1 className='text-gray-700 leading-loose font-bold '>Consulting & Coaching</h1>
-                            <p className='text-sm text-gray-400 font-medium'>Business Strategy , Platform Setup</p>
+                            <p className='text-sm text-gray-400'>Business Strategy , Platform Setup</p>
                         </Link>
                     </li>
                 </ul>
