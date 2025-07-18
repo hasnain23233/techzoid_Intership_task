e19bb9f1f7203df72c2949ab09ed1261f0655cfa
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-07-18 23:22:12 +0500

    full responsive the process page of the website

--- src/page/process.jsx
+++ src/page/process.jsx
@@ -8,16 +8,16 @@ const process = () => {
         <div>
             <div className='h-screen bg-[#f5f8fc] flex items-center justify-center'>
                 <div className='w-full text-center'>
-                    <p className='text-blue-600 p-1 m-auto rounded-full bg-blue-100 w-2/12 gap-4 flex items-center flex-row  justify-center text-sm'> 🔄 Our Proven Process </p>
-                    <h1 className='text-6xl py-2 font-bold leading-tight'>
+                    <p className='text-blue-600 p-1 m-auto rounded-full bg-blue-100 lg:w-2/12 sm:w-8/12 w-11/12 gap-4 flex items-center flex-row  justify-center text-sm'> 🔄 Our Proven Process </p>
+                    <h1 className='xl:text-6xl lg:text-5xl sm:text-4xl text-3xl py-2 font-bold leading-tight'>
                         From Idea to    <span className="bg-gradient-to-r from-[#007BFF] to-[#00FFD1] py-2 bg-clip-text text-transparent">
                             Implementation
                         </span>
                     </h1>
-                    <p className='text-xl w-8/12 leading-snug m-auto text-gray-500 py-3'>
+                    <p className='lg:text-xl sm:text-md text-sm  lg:w-7/12 sm:w-9/12 w-11/12 leading-snug m-auto text-gray-500 py-3'>
                         Our structured 5-step approach ensures successful delivery of your AI and automation projects.
                     </p>
-                    <div className='w-2/12 m-auto mt-3 flex justify-center'>
+                    <div className='lg:w-2/12 sm:w-6/12 w-10/12 m-auto mt-3 flex justify-center'>
                         <button className="bg-gradient-to-r from-[#007BFF] to-[#00FFD1] text-white font-semibold py-3 px-4 rounded-lg shadow-md hover:opacity-90 transition">
                             Start Your Project
                         </button>
@@ -25,8 +25,8 @@ const process = () => {
                 </div>
             </div>
             <div className='w-11/12 m-auto mt-12 pb-12'>
-                <div className='flex items-center justify-between'>
-                    <div className='border py-5 cursor-pointer w-6/12 px-6 hover:shadow-lg rounded-lg'>
+                <div className='grid lg:grid-cols-2 grid-cols-1 items-center justify-between'>
+                    <div className='border py-5 cursor-pointer px-6 hover:shadow-lg rounded-lg'>
                         <div className='flex gap-2 items-center justify-between '>
                             <p className=" bg-gradient-to-r from-[#007BFF] to-[#00FFD1] text-white font-semibold w-16 h-16 rounded-full flex items-center justify-center shadow-md hover:opacity-90 transition">01</p>
                             <div className=' w-10/12'>
@@ -53,14 +53,14 @@ const process = () => {
                             </p>
                         </div>
                     </div>
-                    <div className='w-6/12'>
+                    <div className='lg:block hidden'>
                         <p className=" bg-gradient-to-r m-auto from-[#007bff37] to-[#00ffd02d] text-blue-600 font-semibold w-28 h-28 rounded-full flex items-center justify-center shadow-md hover:opacity-90 transition">
                             <FontAwesomeIcon icon={faUserFriends} size='3x' />
                         </p>
                     </div>
                 </div>
-                <div className='flex items-center justify-between mt-6 flex-row-reverse'>
-                    <div className='border py-5 cursor-pointer w-6/12 px-6 hover:shadow-lg rounded-lg'>
+                <div className='flex items-center justify-between mt-6 lg:flex-row-reverse flex-col'>
+                    <div className='border py-5 cursor-pointer lg:w-6/12 sm:w-10/12 w-full px-6 hover:shadow-lg rounded-lg'>
                         <div className='flex gap-2 items-center justify-between '>
                             <p className=" bg-gradient-to-r from-[#007BFF] to-[#00FFD1] text-white font-semibold w-16 h-16 rounded-full flex items-center justify-center shadow-md hover:opacity-90 transition">02</p>
                             <div className=' w-10/12'>
@@ -88,14 +88,14 @@ const process = () => {
                             </p>
                         </div>
                     </div>
-                    <div className='w-6/12'>
+                    <div className='w-6/12 lg:block hidden'>
                         <p className=" bg-gradient-to-r m-auto from-[#007bff37] to-[#00ffd02d] text-blue-600 font-semibold w-28 h-28 rounded-full flex items-center justify-center shadow-md hover:opacity-90 transition">
                             <FontAwesomeIcon icon={faCheckCircle} size='3x' />
                         </p>
                     </div>
                 </div>
                 <div className='flex items-center justify-between mt-6'>
-                    <div className='border py-5 cursor-pointer w-6/12 px-6 hover:shadow-lg rounded-lg'>
+                    <div className='border py-5 cursor-pointer lg:w-6/12 sm:w-10/12 w-full px-6 hover:shadow-lg rounded-lg'>
                         <div className='flex gap-2 items-center justify-between '>
                             <p className=" bg-gradient-to-r from-[#007BFF] to-[#00FFD1] text-white font-semibold w-16 h-16 rounded-full flex items-center justify-center shadow-md hover:opacity-90 transition">03</p>
                             <div className=' w-10/12'>
@@ -123,14 +123,14 @@ const process = () => {
                             </p>
                         </div>
                     </div>
-                    <div className='w-6/12'>
+                    <div className='w-6/12 lg:block hidden'>
                         <p className=" bg-gradient-to-r m-auto from-[#007bff37] to-[#00ffd02d] text-blue-600 font-semibold w-28 h-28 rounded-full flex items-center justify-center shadow-md hover:opacity-90 transition">
                             <FontAwesomeIcon icon={faBolt} size='3x' />
                         </p>
                     </div>
                 </div>
-                <div className='flex items-center justify-between mt-6 flex-row-reverse'>
-                    <div className='border py-5 cursor-pointer w-6/12 px-6 hover:shadow-lg rounded-lg'>
+                <div className='flex items-center justify-between mt-6 lg:flex-row-reverse flex-col'>
+                    <div className='border py-5 cursor-pointer lg:w-6/12 sm:w-10/12 w-full px-6 hover:shadow-lg rounded-lg'>
                         <div className='flex gap-2 items-center justify-between '>
                             <p className=" bg-gradient-to-r from-[#007BFF] to-[#00FFD1] text-white font-semibold w-16 h-16 rounded-full flex items-center justify-center shadow-md hover:opacity-90 transition">04</p>
                             <div className=' w-10/12'>
@@ -159,14 +159,14 @@ const process = () => {
                             </p>
                         </div>
                     </div>
-                    <div className='w-6/12'>
+                    <div className='w-6/12 lg:block hidden'>
                         <p className=" bg-gradient-to-r m-auto from-[#007bff37] to-[#00ffd02d] text-blue-600 font-semibold w-28 h-28 rounded-full flex items-center justify-center shadow-md hover:opacity-90 transition">
                             <FontAwesomeIcon icon={faArrowRight} size='3x' />
                         </p>
                     </div>
                 </div>
                 <div className='flex items-center justify-between mt-6'>
-                    <div className='border py-5 cursor-pointer w-6/12 px-6 hover:shadow-lg rounded-lg'>
+                    <div className='border py-5 cursor-pointer lg:w-6/12 sm:w-10/12 w-full px-6 hover:shadow-lg rounded-lg'>
                         <div className='flex gap-2 items-center justify-between '>
                             <p className=" bg-gradient-to-r from-[#007BFF] to-[#00FFD1] text-white font-semibold w-16 h-16 rounded-full flex items-center justify-center shadow-md hover:opacity-90 transition">05</p>
                             <div className=' w-10/12'>
@@ -197,7 +197,7 @@ const process = () => {
                             </p>
                         </div>
                     </div>
-                    <div className='w-6/12'>
+                    <div className='w-6/12 lg:block hidden'>
                         <p className=" bg-gradient-to-r m-auto from-[#007bff37] to-[#00ffd02d] text-blue-600 font-semibold w-28 h-28 rounded-full flex items-center justify-center shadow-md hover:opacity-90 transition">
                             <FontAwesomeIcon icon={faClock} size='3x' />
                         </p>
